aae22097ff3bba6ed62d59a0b2d584e1266c3363
alex <82953425+alexmarroig@users.noreply.github.com>
2026-03-04 12:21:30 -0300

    Add worker watchdog with recovery and alerting

--- workers/__init__.py
+++ workers/__init__.py
@@ -1,1 +1,6 @@
 """AgentOS workers package."""
+
+from workers.agent_worker import AgentWorker, WorkerPool
+from workers.watchdog import WorkerWatchdog
+
+__all__ = ["AgentWorker", "WorkerPool", "WorkerWatchdog"]
